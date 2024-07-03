e376d04318df40c9168619e2df5b247959f09b65
li.qi <15868175516@163.com>
2024-07-03 23:26:28 +0800

    test: add ConfigValidatorTest Test
    DataSourcePropertiesModuleValidatorTest
    DynamicJdbcConfigValidatorTest

--- dynamic-tdd/src/test/java/io/microsphere/dynamic/jdbc/spring/boot/config/validation/ConfigValidatorTest.java
+++ dynamic-tdd/src/test/java/io/microsphere/dynamic/jdbc/spring/boot/config/validation/ConfigValidatorTest.java
@@ -1,6 +1,9 @@
 package io.microsphere.dynamic.jdbc.spring.boot.config.validation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.microsphere.dynamic.jdbc.spring.boot.config.DynamicJdbcConfig;
+import io.microsphere.dynamic.jdbc.spring.boot.datasource.validation.DataSourcePropertiesModuleValidator;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -10,8 +13,14 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 class ConfigValidatorTest {
@@ -57,5 +66,89 @@ class ConfigValidatorTest {
         public ConfigurableApplicationContext getApplicationContext() {
             return context;
         }
+    }
+
+
+    @Nested
+    class DynamicJdbcConfigValidatorTest {
+
+        ConfigValidator configValidator = new DynamicJdbcConfigValidator();
+
+        @Test
+        void shouldBeNotValidWhenNameIsNull() throws Exception {
+            DynamicJdbcConfig dynamicJdbcConfig = readConfig("dynamic/jdbc/no-name-test-jdbc-config.json");
+            ValidationErrors validationErrors = new ValidationErrors("test");
+            configValidator.validate(dynamicJdbcConfig, "test", validationErrors);
+            assertFalse(validationErrors.isValid());
+            assertThat(validationErrors.toString())
+                    .contains("must contain 'name' attribute");
+        }
+
+        @Test
+        void shouldBeNotValidWhenNoModule() throws Exception {
+            DynamicJdbcConfig dynamicJdbcConfig = readConfig("dynamic/jdbc/no-module-test-jdbc-config.json");
+            ValidationErrors validationErrors = new ValidationErrors("test");
+            configValidator.validate(dynamicJdbcConfig, "test", validationErrors);
+            assertFalse(validationErrors.isValid());
+            assertThat(validationErrors.toString())
+                    .contains("must contain one of modules");
+        }
+
+        @Test
+        void shouldBeValidWhenNameIsNotNullAndAtLeastOneModule() throws Exception {
+            DynamicJdbcConfig dynamicJdbcConfig = readConfig("dynamic/jdbc/valid-test-jdbc-config.json");
+            ValidationErrors validationErrors = new ValidationErrors("test");
+            configValidator.validate(dynamicJdbcConfig, "test", validationErrors);
+            assertTrue(validationErrors.isValid());
+        }
+    }
+
+    @Nested
+    class DataSourcePropertiesModuleValidatorTest {
+
+        ConfigValidator configValidator = new DataSourcePropertiesModuleValidator();
+
+        @Test
+        void shouldBeValidWhenHasDataSourceModule() throws Exception {
+            DynamicJdbcConfig dynamicJdbcConfig = readConfig("dynamic/jdbc/valid-test-jdbc-config.json");
+            ValidationErrors validationErrors = new ValidationErrors("test");
+            configValidator.validate(dynamicJdbcConfig, "test", validationErrors);
+            assertTrue(validationErrors.isValid());
+        }
+
+        @Test
+        void shouldBeValidWhenHasHighAvailabilityDataSourceModuleWithTwoDatasouceAndDefaultZoneConfig() throws Exception {
+            DynamicJdbcConfig dynamicJdbcConfig = readConfig("dynamic/jdbc/valid-ha-test-jdbc-config.json");
+            ValidationErrors validationErrors = new ValidationErrors("test");
+            configValidator.validate(dynamicJdbcConfig, "test", validationErrors);
+            assertTrue(validationErrors.isValid());
+        }
+
+        @Test
+        void shouldBeNotValidWhenHasNeitherDataSourceAndHighAvailabilityNorDataSourceModule() throws Exception {
+            DynamicJdbcConfig dynamicJdbcConfig = readConfig("dynamic/jdbc/no-datasource-ha-test-jdbc-config.json");
+            ValidationErrors validationErrors = new ValidationErrors("test");
+            configValidator.validate(dynamicJdbcConfig, "test", validationErrors);
+            assertFalse(validationErrors.isValid());
+            assertThat(validationErrors.toString())
+                    .contains("module must be present");
+        }
+
+        @Test
+        void shouldBeNotValidWhenHasBothDataSourceAndHighAvailabilityDataSourceModule() throws Exception {
+            DynamicJdbcConfig dynamicJdbcConfig = readConfig("dynamic/jdbc/both-datasource-ha-test-jdbc-config.json");
+            ValidationErrors validationErrors = new ValidationErrors("test");
+            configValidator.validate(dynamicJdbcConfig, "test", validationErrors);
+            assertFalse(validationErrors.isValid());
+            assertThat(validationErrors.toString())
+                    .contains("module must not be present at the same time");
+        }
+    }
+
+    private DynamicJdbcConfig readConfig(String configFile) throws Exception {
+
+        URL resource = this.getClass().getClassLoader().getResource(configFile);
+        String content = IOUtils.toString(resource, StandardCharsets.UTF_8);
+        return new ObjectMapper().readValue(content, DynamicJdbcConfig.class);
     }
 }
